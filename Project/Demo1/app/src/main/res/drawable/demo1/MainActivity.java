3547fe4bb7afbbfc0183ff410eae73c2e4b09c84
thientm-denk <denkhotieu@gmail.com>
2023-05-16 09:42:43 +0700

    new lab and docx

--- Project/Demo1/app/src/main/res/drawable/demo1/MainActivity.java
+++ Project/Demo1/app/src/main/res/drawable/demo1/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.demo_linear_1);
+        setContentView(R.layout.linear_layout);
     }
 }
